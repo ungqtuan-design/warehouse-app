9cf772fff505660b588bf6c262917e6d98e6e977
maciwadee <quoc.viet.ung@intel.com>
2026-07-29 20:55:48 +0700

    Fix Vercel output tracing

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
-import path from "node:path";
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  outputFileTracingRoot: path.join(__dirname, ".."),
 };
 
 export default nextConfig;
